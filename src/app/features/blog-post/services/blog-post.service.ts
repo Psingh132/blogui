7f2caeadc914a81942ddae663600b87492a61856
Priyadarshi Kumar <Priyadarshikumar824@gmail.com>
2024-11-15 06:41:33 +0530

    add updateBlogPost UI

--- src/app/features/blog-post/services/blog-post.service.ts
+++ src/app/features/blog-post/services/blog-post.service.ts
@@ -4,6 +4,7 @@ import { AddBlogPost } from '../models/add-blog-post.model';
 import { Observable } from 'rxjs';
 import { BlogPost } from '../models/blog-post.model';
 import { environment } from 'src/environments/environment';
+import { UpdateBlogPost } from '../models/update-blog-post.model';
 
 @Injectable({
   providedIn: 'root'
@@ -18,5 +19,13 @@ export class BlogPostService {
 
   getAllBlogPosts() : Observable<BlogPost[]> {
     return this.httpClient.get<BlogPost[]>(`${environment.apiBaseUrl}/api/blogposts`);
+  }
+
+  getBlogPostById(id:string): Observable<BlogPost> {
+    return this.httpClient.get<BlogPost>(`${environment.apiBaseUrl}/api/blogposts/${id}`);
+  }
+
+  updateBlogPost(id:string, updateBlogPost: UpdateBlogPost): Observable<BlogPost> {
+    return this.httpClient.put<BlogPost>(`${environment.apiBaseUrl}/api/blogposts/${id}`, updateBlogPost);
   }
 }
